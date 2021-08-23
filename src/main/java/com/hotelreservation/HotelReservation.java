9c6365f12356c31dd58b1bc4e87b21c80c244764
vismayatk002 <vismayatk002@gmail.com>
2021-08-23 08:52:02 +0530

    Added welcome message

---
+++ src/main/java/com/hotelreservation/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.hotelreservation;
+
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation Program" );
+    }
+}
